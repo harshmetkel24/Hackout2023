6aa2efa6ab2053ff3e7c8aae7bb553878392cbf6
Nikhil Vaghasiya <nikhilvaghasiya1600@gmail.com>
2023-08-26 17:05:59 +0530

    Link updated

--- PandemicManagementSystem/server.js
+++ PandemicManagementSystem/server.js
@@ -10,7 +10,7 @@ const PORT = process.env.PORT||8000;
 
 connectDB();
 PMS.use(cors({
-    origin: 'http://localhost:3000',
+    origin: 'https://hackout2023.vercel.app/',
     credentials: true // some legacy browsers (IE11, various SmartTVs) choke on 204
 }))
 
